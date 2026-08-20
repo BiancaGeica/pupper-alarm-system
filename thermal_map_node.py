7771abdb634f5070ecb9c6965efdfacd47312ff6
Elena-Bianca Geica <geicaelenabianca@gmail.com>
2026-08-20 22:55:41 +0300

    Add toxic gas clouds in rviz simulation

--- thermal_map_node.py
+++ thermal_map_node.py
@@ -21,6 +21,11 @@ class ThermalMapNode(Node):
         self.marker_publisher = self.create_publisher(MarkerArray, '/thermal_map_markers', 10) # publisher for RVizz
         self.thermal_data = [] # list of tuples: (x, y, temp)
 
+        self.gas_subscription = self.create_subscription(String, '/alarm_event', self.gas_callback, 10) # listen for gas readings
+        self.gas_marker_publisher = self.create_publisher(MarkerArray, '/gas_map_markers', 10) # publish the clouds in rviz
+        self.gas_data = [] # list of positions of gas clouds (x, y)
+
+
     def get_color(self, temp):
         color = ColorRGBA()
         color.a = 0.3 # semitransparent to mix on the map
@@ -94,6 +99,65 @@ class ThermalMapNode(Node):
 
         except Exception as e:
             pass
+
+    def gas_callback(self, msg):
+        if "POSIBILA SCURGERE DE GAZE" in msg.data:
+            try:
+                gas_level_str = msg.data.split("Nivel gaz: ")[1]
+                gas_level = float(gas_level_str)
+
+                transform = self.tf_buffer.lookup_transform('map', 'base_link', rclpy.time.Time())
+                x = transform.transform.translation.x
+                y = transform.transform.translation.y
+
+                should_save = True
+                if len(self.gas_data) > 0:
+                    last_x, last_y, _ = self.gas_data[-1]
+                    distance = math.sqrt((x - last_x)**2 + (y - last_y)**2)
+                    if distance < 0.4:
+                        should_save = False
+                
+                if should_save:
+                    self.gas_data.append((x, y, gas_level))
+                    self.get_logger().info(f'Gaz detectat ({gas_level})! Nor creat la: ({x:.2f}, {y:.2f})')
+                    
+                marker_array = MarkerArray()
+                for i, data_point in enumerate(self.gas_data):
+                    px, py, pgas = data_point
+                    
+                    marker = Marker()
+                    marker.header.frame_id = 'map'
+                    marker.header.stamp = self.get_clock().now().to_msg()
+                    marker.ns = 'gas_map'
+                    marker.id = i
+                    marker.type = Marker.SPHERE
+                    marker.action = Marker.ADD
+                    
+                    marker.pose.position.x = px
+                    marker.pose.position.y = py
+                    marker.pose.position.z = 0.5
+                    
+                    marker.scale.x = 1.0
+                    marker.scale.y = 1.0
+                    marker.scale.z = 1.0
+                    
+                    marker.color.r = 0.6
+                    marker.color.g = 0.0
+                    marker.color.b = 1.0
+                    
+                    opacity = 0.3 + ((pgas - 300) / 723.0) * 0.7
+                    
+                    if opacity > 1.0: opacity = 1.0
+                    if opacity < 0.1: opacity = 0.1
+                    
+                    marker.color.a = float(opacity)
+                    
+                    marker_array.markers.append(marker)
+
+                self.gas_marker_publisher.publish(marker_array)
+                
+            except Exception as e:
+                pass
 
 def main(args=None):
     rclpy.init()
